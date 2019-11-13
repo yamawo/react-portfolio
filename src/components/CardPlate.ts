a57a3d80c973b85405faeb3327de80822281a51d
yamawo <y7a3m1asaki@gmail.com>
2019-11-14 03:00:07 +0900

    created CardPlate.ts

---
+++ src/components/CardPlate.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const CardPlate = styled.div`
+    display: flex;
+    position: relative;
+    line-height: 1.6em;
+    width: 800px;
+    height: 420px;
+    box-shadow: 0 1px 8px rgba(0, 0, 0, 0.4);
+    border-radius: 3px;
+    background-color: #eaf4fc;
+    padding: 28px;
+    box-sizing: border-box;
+`;
+
+export default CardPlate;
