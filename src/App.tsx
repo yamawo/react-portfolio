83ac01db9ccb04b1e57bf4ee3ee4637d1b819685
yamawo <y7a3m1asaki@gmail.com>
2019-11-13 19:31:26 +0900

    added module by npm, Created TopContainer.tsx, Fixed App.tsx, index.tsx, index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import styled from 'styled-components';
+import { Reset } from 'styled-reset';
+import TopContainer from './containers/TopContainer';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    <Wrapper>
+        <Reset />
+        <TopContainer />
+    </Wrapper>
+};
+
+const Wrapper = styled.div`
+    overflow: hidden;
+`;
 
 export default App;
---
+++ src/containers/TopContainer.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import ProfileCard from '../components/ProfileCard';
+
+const TopContainer: React.FC = () =>{
+    <Top>
+        <ProfileCard />
+    </Top>
+}
+
+const Top = styled.div`
+    width: 100vw;
+    height: 100vh;
+    padding-top: 10%;
+    padding-bottom: 10%;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    background-color: #19448e;
+`;
+
+export default TopContainer;
